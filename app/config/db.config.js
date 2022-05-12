5108f7e94d35ce4aa19a62f5aed83e8b054ddd14
ojcaje <ojcaje@gmail.com>
2022-05-11 20:06:59 -0400

    Initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+
+    HOST: "localhost",
+
+    USER: "postgres",
+
+    PASSWORD: "postgres",
+
+    PORT: 5432,
+
+    DB: "dbpwb",
+
+    dialect: "postgres",
+
+    pool: {
+
+        max: 5,
+
+        min: 0,
+
+        acquire: 30000,
+
+        idle: 10000
+
+    }
+
+};
